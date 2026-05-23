0f27ef3f1497459af894a7d1643d75bc85a7d70f
valdez-jose <tu-email-de-github@example.comclear>
2026-05-23 12:03:50 -0300

    completando estructura angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { Navbar } from './components/navbar/navbar';
 import { Header } from "./components/header/header";
 import { Cards } from './components/cards/cards';
-import { Navbar } from "./components/navbar/navbar";
+import { BicisCards } from './components/bicis-cards/bicis-cards'; 
+import {Locales } from './components/locales/locales';
+import {Contacto} from './components/contacto/contacto';
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Cards, Navbar], 
+  imports: [Header, Cards, BicisCards, Navbar, Locales, Contacto, Footer], 
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
